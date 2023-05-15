35e2fc0d6ded70d17fb1fecd8e7b462d7f475370
Mauricio Alvarado <mau.alvalop43@gmail.com>
2023-05-15 16:17:50 -0600

    Add related products by ECLAT

--- src/components/products/RelatedProducts.js
+++ src/components/products/RelatedProducts.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import View from "components/products/RelatedProducts.view";
 import { useQuery } from "seed/gql";
+import { useGet, usePost } from "seed/api";
 
 function RelatedProducts({ product }) {
   
@@ -9,39 +10,22 @@ function RelatedProducts({ product }) {
   const pageLimit = 10;
   const [pageIndex, setPageIndex] = useState(0);
   const category = product.category.name;
-  const qRelatedProducts = useQuery(`
-    {
-      products {
-        id
-        name
-        shortDescription
-        variants {
-          price
-          photos {
-            id
-            url
-          }
-        }
-        opinions {
-          title
-          description
-          rate
-          user {  }
-        }
-      }
-    }
-  `, `((category.name=${category}) AND (id<>${product.id}))`, { limit: pageLimit });
+  const qRelatedProducts = useGet("/products/get_related_products", {"product_id": product.id});
 
   if(qRelatedProducts.loading) return <div>Loading...</div>;
   if(qRelatedProducts.error) return <div>Error</div>;
 
   const { products } = qRelatedProducts.data;
 
+  let relatedProducts = [];
+  for(let i = 0; i < products.length && relatedProducts.length < pageLimit; i++)
+      relatedProducts.push(products[i]);
+
   return <View 
     pageIndex={pageIndex}
     pageSize={pageSize}
     setPageIndex={setPageIndex}
-    products={products}
+    products={relatedProducts}
   />;
   
 }
