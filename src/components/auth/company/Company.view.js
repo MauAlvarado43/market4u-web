94c2b81b27fad85f04a14115051b5a15a53db853
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-06-11 19:17:11 -0600

    small ui change (yup error style)

--- src/components/auth/company/Company.view.js
+++ src/components/auth/company/Company.view.js
@@ -84,7 +84,7 @@ const CompanyView = ({ companySchema, onSubmit }) =>
 
             {
               errors.email && (touched.email || submitCount > 0)
-                ? <div class="mt-3 alert alert-soft-danger" role="alert">
+                ? <div class="mt-2 text-danger" role="alert">
                   {errors.email}
                 </div>
                 : null
