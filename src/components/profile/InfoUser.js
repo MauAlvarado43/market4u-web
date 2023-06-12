5f7fc1952c54eaeb667951636d9a343972c73e08
AlexDsJz <cduenasj1700@alumno.ipn.mx>
2023-06-11 19:58:08 -0600

    Add validations in profile user

--- src/components/profile/InfoUser.js
+++ src/components/profile/InfoUser.js
@@ -8,7 +8,6 @@ import swal from "sweetalert";
 import { object, string } from "yup";
 
 function InfoUser() {
-  const [selectedFile, setSelectedFile] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
   const [showPassConfirm, setShowPassConfirm] = useState(false);
   const [password, setPassword] = useState("");
@@ -54,6 +53,11 @@ function InfoUser() {
     "id = " + sessionStorage.getItem("id")
   );
 
+  if (reqUsers.loading) return <Loading />;
+  if (reqUsers.error) return "Error";
+
+  const { users } = reqUsers.data;
+
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
@@ -62,42 +66,80 @@ function InfoUser() {
     setShowPassConfirm(!showPassConfirm);
   };
 
-  if (reqUsers.loading) return <Loading />;
-  if (reqUsers.error) return "Error";
-
-  const { users } = reqUsers.data;
-
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-    if (event.target.value !== "") {
-      document.getElementById("passConfirm").disabled = false;
-    } else {
-      document.getElementById("passConfirm").disabled = true;
-      document.getElementById("passConfirm").value = "";
-    }
-  };
-
-  const validatePassword = (pass) => {
-    if(pass.length < 8) 
-      return false;
-    else
-      return true;
   };
 
   const validateLetters = (e) => {
     const keyCode = e.keyCode || e.which;
     const keyValue = String.fromCharCode(keyCode);
-    const regex = /^[A-Za-z\s]+$/; 
+    const regex = /^[A-Za-záéíóúÁÉÍÓÚñÑ\s]+$/;
 
     if (!regex.test(keyValue)) 
         e.preventDefault();
   }
 
   const validationSchema = object({
-    password: string().test({
-      name: "passConfirm",
+    firstName: string().test({
+      name: "firstName",
       test(value, context) {
-        if(!validatePassword(password) && (password))
+        if(!value)
+          return context.createError({
+            message: "Ingresa tu nombre",
+          });
+
+        if (value.length < 3)
+          return context.createError({
+            message: "El nombre debe tener al menos 3 caracteres.",
+          });
+
+        return true;
+      },
+    }),
+    lastName: string().test({
+      name: "lastName",
+      test(value, context) {
+        if(!value)
+          return context.createError({
+            message: "Ingresa tu apellido",
+          });
+        
+        if (value.length < 3) 
+          return context.createError({
+            message: "El apellido debe tener al menos 3 caracteres.",
+          });
+        
+        return true;
+      },
+    }),
+    telephone: string().test({
+      name: "telephone",
+      test(value, context) {
+        if(document.getElementById("telephone").value)
+          if(value.length !== 10)
+            return context.createError({
+              message: "El teléfono debe tener 10 dígitos.",
+            });
+          
+          return true;
+      },
+    }),
+    cp: string().test({
+      name: "cp",
+      test(value, context) {
+        if(document.getElementById("cp").value)
+          if(value.length !== 5)
+            return context.createError({
+              message: "El código postal debe tener 5 dígitos.",
+            });
+            
+          return true;
+      },
+    }),
+    password: string().test({
+      name: "password",
+      test(value, context) {
+        if(password.length < 8 && password !== "")
           return context.createError({
             message: "La contraseña debe tener una longitud de 8 caracteres."
           });
@@ -107,6 +149,11 @@ function InfoUser() {
     passwordConfirm: string().test({
       id:"passConfirm",
       test(value, context) {
+        if(password !== "" && passwordConfirm === "")
+          return context.createError({
+            message: "Confirma tu contraseña."
+          });
+
         if (passwordConfirm !== password) {
           return context.createError({
             message: "Las contraseñas no coinciden.",
@@ -120,17 +167,11 @@ function InfoUser() {
   const onSubmit = (values) => {
     let newValues = JSON.parse(JSON.stringify(values));
     newValues.user_id = newValues.id;
-
-    if(!password) {
-      delete newValues.id;
-      newValues.password = password;
-      callUpdateUser(newValues);
-    } else {
-      validationSchema.validate({ password }); 
-      delete newValues.id;
-      newValues.password = password;
-      callUpdateUser(newValues);
-    }
+    delete newValues.id;
+    delete newValues.email;
+    newValues.password = password;
+    
+    callUpdateUser(newValues);
   };
 
   return (
@@ -141,7 +182,6 @@ function InfoUser() {
       setPassword={setPassword}
       showPassword={showPassword}
       showPassConfirm={showPassConfirm}
-      setSelectedFile={setSelectedFile}
       validateLetters={validateLetters}
       validationSchema={validationSchema}
       setPasswordConfirm={setPasswordConfirm}
